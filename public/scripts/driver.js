aac8e0057473980b59fbf998bbc84997989ea520
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-02 19:47:28 -0700

    trying to solve image freeze problem

--- public/scripts/driver.js
+++ public/scripts/driver.js
@@ -1,13 +1,14 @@
 $(() => {
-
-  window.fishtank = new Fishtank('fishtank');             // making it global to make debugging 1% easier.  shhhh.
-  window.fishtank.registerSpecies(SwitchFish, GoFish, BiteFish, BigBoy, FastBoy);
+  window.fishtank = new Fishtank("fishtank"); // making it global to make debugging 1% easier.  shhhh.
+  window.fishtank.registerSpecies(
+    SwitchFish,
+    GoFish,
+    BiteFish,
+    BigBoy,
+    FastBoy
+  );
   var starter = new Starter({
-    tank: window.fishtank,     // look, it's Dependency Injection!  Pretend you care!
-    position: new Vector(0, 0)
+    tank: window.fishtank, // look, it's Dependency Injection!  Pretend you care!
+    position: new Vector(0, 0),
   });
-
 });
-
-
-
